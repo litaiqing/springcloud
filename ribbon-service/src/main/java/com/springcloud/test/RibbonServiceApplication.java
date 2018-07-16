10c77e7a73d833ef5ceb2cd1a450ecb8875ff265
ltqaaa <ltqaaa@qq.com>
2018-07-16 17:02:05 +0800

    使用RestTemplate客户端进行负载均衡

---
+++ ribbon-service/src/main/java/com/springcloud/test/RibbonServiceApplication.java
@@ -0,0 +1,34 @@
+package com.springcloud.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Description:
+ * <br/>
+ *
+ * @author litaiqing
+ * @version 1.0
+ * @date: 2018/7/16 16:21
+ * @since JDK 1.8
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class RibbonServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonServiceApplication.class, args);
+    }
+
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
