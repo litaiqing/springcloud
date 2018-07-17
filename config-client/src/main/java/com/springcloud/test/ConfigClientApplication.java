68972d1e1fdb0fc3370937a537c41d3c3acee524
ltqaaa <ltqaaa@qq.com>
2018-07-17 15:27:58 +0800

    Spring Cloud BUS 消息总线

--- config-client/src/main/java/com/springcloud/test/ConfigClientApplication.java
+++ config-client/src/main/java/com/springcloud/test/ConfigClientApplication.java
@@ -3,6 +3,8 @@ package com.springcloud.test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
  * @date: 2018/7/17 11:39
  * @since JDK 1.8
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @RestController
+@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
--- config-server/src/main/java/com/springcloud/test/ConfigServerApplication.java
+++ config-server/src/main/java/com/springcloud/test/ConfigServerApplication.java
@@ -3,6 +3,7 @@ package com.springcloud.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Description:
@@ -15,6 +16,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
